dc2253bb092586fc91a8c3bd1128ff4ee10c5ca7
yaozeyu112 <3484766443@qq.com>
2022-10-01 21:15:21 +0800

    Initial commit

---
+++ src/main/java/com/example/jenkinsdemo/JenkinsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsdemoApplication.class, args);
+	}
+
+}
