268593aed8ea6cf4e4d627e3a900f659ee80eadf
yaozeyu112 <3484766443@qq.com>
2022-10-10 23:52:01 +0800

    feat: 冒泡排序

---
+++ src/main/java/com/example/jenkinsdemo/demo2/BubbleSort.java
@@ -0,0 +1,28 @@
+package demo2;
+
+/**
+ * 冒泡排序
+ *
+ * @author chiclaim
+ * @see <a href="https://github.com/chiclaim/">github</a>
+ * @since 2022/9/4
+ */
+public class BubbleSort implements ISort{
+
+    @Override
+    public void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            boolean swapped = false;
+            // 每一趟：当前元素和下一个元素对比
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+}
---
+++ src/main/java/com/example/jenkinsdemo/demo2/ISort.java
@@ -0,0 +1,5 @@
+package demo2;
+
+public interface ISort {
+    void sort(int[] arr);
+}
