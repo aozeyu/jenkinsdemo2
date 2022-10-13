c766a36dbec43dfa739494857e2ccc07aab9ccad
yaozeyu112 <3484766443@qq.com>
2022-10-13 20:40:10 +0800

    feat: 2121

---
+++ src/main/java/com/example/jenkinsdemo/dubbo/RunoobTest.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsdemo.dubbo;
+
+public class RunoobTest {
+    public static void main(String[] args) {
+        String str = new String("This is Text");
+        System.out.println("返回值 :");
+        System.out.println(str.substring(4));
+
+        System.out.println("返回值 :");
+        System.out.println(str.substring(4,10));
+    }
+}
