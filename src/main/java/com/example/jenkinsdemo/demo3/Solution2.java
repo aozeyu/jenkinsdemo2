5b81287d36843d6d7f7b0505d9fb410efefaa7d8
yaozeyu112 <3484766443@qq.com>
2022-10-15 13:52:59 +0800

    feat: 反射

---
+++ src/main/java/com/example/jenkinsdemo/demo3/Solution2.java
@@ -0,0 +1,24 @@
+package com.example.jenkinsdemo.demo3;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+        s.close();
+        HashSet<String> set = new HashSet<>(t);
+        for (int i = 0; i < t; i++) {
+            set.add(pair_left[i] + " " + pair_right[i]);
+            System.out.println(set.size());
+        }
+
+    }
+}
