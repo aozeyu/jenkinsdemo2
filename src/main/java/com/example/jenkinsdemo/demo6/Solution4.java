af22a3c6e200ac2a0da225ea61fb549c9ade3782
aozeyu <3484766443@qq.com>
2022-11-12 16:31:27 +0800

    feat:   二分查找

---
+++ src/main/java/com/example/jenkinsdemo/demo6/Solution4.java
@@ -0,0 +1,24 @@
+package com.example.jenkinsdemo.demo6;
+
+/**
+ * @author yaozeyu
+ */
+public class Solution4 {
+  public int[] searchRange(int[] nums, int target) {
+      int l = search(nums,target);
+      int r = search(nums,target + 1);
+      return  l == r ? new int[] {-1, 1}: new int[] {l, r-1};
+  }
+  private int search(int[] nums, int x) {
+    int left = 0, right = nums.length;
+    while (left < right) {
+      int mid = (left + right) >>> 1;
+      if (nums[mid] >= x) {
+        right = mid;
+      }else  {
+        left = mid + 1;
+      }
+    }
+    return left;
+  }
+}
