3b4240c14b98c7de0e83537d4a48066b22beabfa
aozeyu <3484766443@qq.com>
2022-11-13 17:22:08 +0800

    feat:  jjj

--- src/main/java/com/example/jenkinsdemo/demo7/MD5Util.java
+++ src/main/java/com/example/jenkinsdemo/demo7/MD5Util.java
@@ -1,12 +1,23 @@
 package com.example.jenkinsdemo.demo7;
 
+import com.sun.javafx.scene.traversal.Algorithm;
+import sun.misc.BASE64Decoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+
+import java.io.IOException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author yaozeyu
  */
 public class MD5Util {
   public static final String KEY_MD5 = "MD5";
+  private static String KEY_STR="myKey";
+  private static String CHARSETNAME="UTF-8";
+  private static String ALGORITHM="DES";
   /***
    * MD5加密（生成唯一的MD5值）
    */
@@ -14,5 +25,20 @@ public class MD5Util {
     MessageDigest md5 = MessageDigest.getInstance(KEY_MD5);
     md5.update(data);
     return md5.digest();
+  }
+  /***
+   * 获取解密之后的信息
+   */
+  public static String getDecryptString(String str) {
+    BASE64Decoder decoder = new BASE64Decoder();
+    try {
+      //将字符串decode成byte[]
+      byte[] bytes = decoder.decodeBuffer(str);
+      //获取解密对象
+      Cipher cipher = Cipher.getInstance(ALGORITHM);
+    } catch (IOException | NoSuchPaddingException | NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+    return str;
   }
 }
