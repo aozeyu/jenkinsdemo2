7325c679b0ca402b6aaa5fc30fbe650d92a00913
aozeyu <3484766443@qq.com>
2022-11-28 22:43:15 +0800

    feat: optional

--- src/main/java/com/example/jenkinsdemo/javaDesign/App.java
+++ src/main/java/com/example/jenkinsdemo/javaDesign/App.java
@@ -1,16 +1,18 @@
 package com.example.jenkinsdemo.javaDesign;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class App {
-  public Optional<Integer> findElement(Integer[] arr, int x) {
-    //找数组中的元素
-    for (Integer num : arr) {
-      if (num == x) {
-        return  Optional.of(x) ;
-      }
-    }
-    return Optional.empty();
+  public static Optional<Integer> findElement(Integer[] arr, int x) {
+    return Arrays.stream(arr).filter(num -> num == x).findFirst();
+//    //找数组中的元素
+//    for (Integer num : arr) {
+//      if (num == x) {
+//        return  Optional.of(x) ;
+//      }
+//    }
+//    return Optional.empty();
   }
 
   public static void main(String[] args) {
@@ -22,7 +24,10 @@ public class App {
 //    if (numDoesNotExists.isPresent()) {
 //      System.out.println(numDoesNotExists.get());
 //    }
-    numExists.ifPresent(System.out::println);
-    numDoesNotExists.ifPresent(System.out::println);
+//    numExists.ifPresent(System.out::println);
+//    numDoesNotExists.ifPresent(System.out::println);
+
+    findElement(new Integer[]{1,2,3,4,5},3).ifPresent(System.out::println);
+    findElement(new Integer[]{1,2,3,4,5},6).ifPresent(System.out::println);
   }
 }
