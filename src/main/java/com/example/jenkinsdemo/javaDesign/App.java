10180f29c71398ad3dc9e163d063c010001d0382
aozeyu <3484766443@qq.com>
2022-11-28 22:37:45 +0800

    feat: optional

--- src/main/java/com/example/jenkinsdemo/javaDesign/App.java
+++ src/main/java/com/example/jenkinsdemo/javaDesign/App.java
@@ -3,24 +3,26 @@ package com.example.jenkinsdemo.javaDesign;
 import java.util.Optional;
 
 public class App {
-  public Integer findElement(Integer[] arr, int x) {
+  public Optional<Integer> findElement(Integer[] arr, int x) {
     //找数组中的元素
     for (Integer num : arr) {
       if (num == x) {
-        return  x ;
+        return  Optional.of(x) ;
       }
     }
-    return null;
+    return Optional.empty();
   }
 
   public static void main(String[] args) {
     Optional<Integer> numExists = Optional.of(5);
     Optional<Integer> numDoesNotExists = Optional.empty();
-    if (numExists.isPresent()) {
-      System.out.println(numExists.get());
-    }
-    if (numDoesNotExists.isPresent()) {
-      System.out.println(numDoesNotExists.get());
-    }
+//    if (numExists.isPresent()) {
+//      System.out.println(numExists.get());
+//    }
+//    if (numDoesNotExists.isPresent()) {
+//      System.out.println(numDoesNotExists.get());
+//    }
+    numExists.ifPresent(System.out::println);
+    numDoesNotExists.ifPresent(System.out::println);
   }
 }
