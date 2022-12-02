4b7b568ae365c705ea81d1ae7ce3980c951c5985
aozeyu <3484766443@qq.com>
2022-12-02 15:31:37 +0800

    feat: optional

---
+++ src/main/java/com/example/jenkinsdemo/javaDesign/Main.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsdemo.javaDesign;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  private static final List<String> URL_LIST = new ArrayList<>();
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+
+  }
+}
